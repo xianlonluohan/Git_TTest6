d0bbbfc6f18bdc4edf4b1ea1091e4711ad94175c
zxcv <zxcv@qq.com>
2022-10-29 23:25:38 +0800

    second commit

--- src/main/java/Hjui/hello.java
+++ src/main/java/Hjui/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word2");
+        System.out.println("hello word3");
     }
 }
